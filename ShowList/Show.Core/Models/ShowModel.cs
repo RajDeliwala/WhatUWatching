46620e509400bd9583794afa986604acffe26880
Raj Deliwala <rajdeliwalaa@gmail.com>
2020-09-01 14:47:32 -0500

    Show model and Inmemory show repo built

---
+++ ShowList/Show.Core/Models/ShowModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Show.Core.Models
+{
+    public class ShowModel
+    {
+        /** Show variables that need to be recorded **/
+
+        //Unique Id per show
+        public string Id { get; set; }
+        //Name of the show
+        [DisplayName("Show Name")]
+        public string Name { get; set; }
+        //Breif description of the show
+        public string Description { get; set; }
+        //Genere of the show
+        public string Genere1 { get; set; }
+        //Genere of the show
+        public string Genere2 { get; set; }
+        //Genere of the show
+        public string Genere3 { get; set; }
+        //Genere of the show
+        public string Genere4 { get; set; }
+        //Genere of the show
+        public string Genere5 { get; set; }
+        //Image of the show
+        public string Image { get; set; }
+        //What season the show premiered in
+        public string PremieredSeason { get; set; }
+        //Number of episodes
+        public string EpisodeCount { get; set; }
+        //Studio that is working on the show
+        public string Studio { get; set; }
+
+
+        //Constructor to initialize a random ID per new product
+        public ShowModel()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ ShowList/Show.DataAccess.InMemory/ShowRepo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Caching;
+using Show.Core.Models;
+
+namespace Show.DataAccess.InMemory
+{
+    public class ShowRepo
+    {
+        //Base needs
+        ObjectCache cache = MemoryCache.Default;
+        List<ShowModel> shows;
+
+        //Constructor
+        public ShowRepo()
+        {
+            shows = cache["shows"] as List<ShowModel>;
+            if (shows == null)
+            {
+                shows = new List<ShowModel>();
+            }
+        }
+
+        //Commit function
+        public void Commit()
+        {
+            cache["shows"] = shows;
+        }
+
+        //Insert function
+        public void Insert(ShowModel s)
+        {
+            shows.Add(s);
+        }
+
+        //Update function
+        public void Update(ShowModel s)
+        {
+            ShowModel showToUpdate = shows.Find(p => p.Id == s.Id);
+            if (showToUpdate != null)
+            {
+                showToUpdate = s;
+            }
+            else
+            {
+                throw new Exception("Show not found");
+            }
+        }
+
+        //Find function
+        public ShowModel Find(string Id)
+        {
+            ShowModel show = shows.Find(p => p.Id == Id);
+            if (show != null)
+            {
+                return show;
+            }
+            else
+            {
+                throw new Exception("Show not found");
+            }
+        }
+
+        //Queryable collection fucntion
+        public IQueryable<ShowModel> Collection()
+        {
+            return shows.AsQueryable();
+        }
+
+        //Delete show function
+        public void Delete(string Id)
+        {
+            ShowModel showToDelete = shows.Find(p => p.Id == Id);
+            if(showToDelete != null)
+            {
+                shows.Remove(showToDelete);
+            }
+            else
+            {
+                throw new Exception("Show no found");
+            }
+        }
+
+    }
+}
