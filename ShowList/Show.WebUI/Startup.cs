a4556069ea6a41afb8cf5c3089e77779db731720
Raj Deliwala <rajdeliwalaa@gmail.com>
2020-09-01 13:27:48 -0500

    Inital solution set up

---
+++ ShowList/Show.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Show.WebUI.Startup))]
+namespace Show.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
