f63b26acc945a4642a0e4ee921b9274c6abd1202
Raj Deliwala <rajdeliwalaa@gmail.com>
2020-09-03 14:41:29 -0500

    Adding product listing on main pages

--- ShowList/Show.WebUI/Controllers/HomeController.cs
+++ ShowList/Show.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Show.Core.Contracts;
+using Show.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +10,36 @@ namespace Show.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        // Getting context
+        IRepo<ShowModel> context;
+        IRepo<ShowSeason> showseasonContext;
+
+        // Default constructor
+        public HomeController(IRepo<ShowModel> showContext, IRepo<ShowSeason> showSeasonContext)
+        {
+            context = showContext;
+            showseasonContext = showSeasonContext;
+        }
+
+        //View product function
+        public ActionResult Details(string Id)
+        {
+            ShowModel shows = context.Find(Id);
+            if(shows == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(shows);
+            }
+        }
+
+
         public ActionResult Index()
         {
-            return View();
+            List<ShowModel> shows = context.Collection().ToList();
+            return View(shows);
         }
 
         public ActionResult About()
