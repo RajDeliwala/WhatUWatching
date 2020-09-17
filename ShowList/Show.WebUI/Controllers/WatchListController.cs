5383f2a4f41b597cb646b8c30902a6005b6cc130
Raj Deliwala <rajdeliwalaa@gmail.com>
2020-09-17 13:50:13 -0500

    linked customers to watch list models

--- ShowList/Show.WebUI/Controllers/WatchListController.cs
+++ ShowList/Show.WebUI/Controllers/WatchListController.cs
@@ -13,13 +13,15 @@ namespace Show.WebUI.Controllers
 
         IWatchListService watchListService;
         IOrderWatchList orderWatchListService;
+        IRepo<Customer> customers;
         
 
         //Default Constructor
-        public WatchListController(IWatchListService WatchListService, IOrderWatchList OrderWatchList)
+        public WatchListController(IWatchListService WatchListService, IOrderWatchList OrderWatchList, IRepo<Customer> Customers)
         {
             this.watchListService = WatchListService;
             this.orderWatchListService = OrderWatchList;
+            this.customers = Customers;
         }
         // GET: WatchList
         public ActionResult Index()
@@ -42,16 +44,35 @@ namespace Show.WebUI.Controllers
         }
 
         //Return Checkout
+        [Authorize]
         public ActionResult Checkout()
         {
-            return View();
+            Customer customer = customers.Collection().FirstOrDefault(c => c.Email == User.Identity.Name);
+            if(customer != null)
+            {
+                OrderWatchList Order = new OrderWatchList()
+                {
+                    Email = customer.Email,
+                    FirstName = customer.FirstName,
+                    LastName = customer.LastName
+                };
+
+                return View(Order);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+            
         }
 
+        [Authorize]
         [HttpPost]
         public ActionResult Checkout(OrderWatchList OrderWatchList)
         {
             var wishListItems = watchListService.GetWatchListItems(this.HttpContext);
             OrderWatchList.ListStatus = "List Created";
+            OrderWatchList.Email = User.Identity.Name;
 
             orderWatchListService.CreateOrderWatchList(OrderWatchList, wishListItems);
             watchListService.ClearWatchList(this.HttpContext);
